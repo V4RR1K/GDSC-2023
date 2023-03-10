5d7f17fd886b735fa0eccfb606074132cc355b5b
Helen Katherine <helenkatherine@Helens-MacBook-Pro.local>
2023-03-10 18:46:10 -0500

    Helen: navbar trial fail

--- Frontend/angular-recycleRaccoon/src/app/navigation-bar/navigation-bar.component.ts
+++ Frontend/angular-recycleRaccoon/src/app/navigation-bar/navigation-bar.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-navigation-bar',
-    templateUrl: './navigation-bar.component.html',
-    styleUrls: ['./navigation-bar.component.css']
+    templateUrl: '/navigation-bar.component.html',
+    styleUrls: ['/navigation-bar.component.css']
 })
     
 export class NavigationBarComponent{
