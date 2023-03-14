824c0d7fbaf272df37e6ae6cbfc9ef4550218558
Greg Lynskey <gclynskey@outlook.com>
2023-03-14 12:41:49 -0400

    Greg: Stubbed out FileDAO

--- Backend/RecycleRacoonAPI/src/main/java/com/recycleracoon/RecycleRacoonAPI/Persistence/RecycleNodeFileDAO.java
+++ Backend/RecycleRacoonAPI/src/main/java/com/recycleracoon/RecycleRacoonAPI/Persistence/RecycleNodeFileDAO.java
@@ -4,7 +4,65 @@ import com.recycleracoon.RecycleRacoonAPI.Model.RecycleNode;
 
 import java.io.IOException;
 
-public class RecycleNodeFileDAO {
+public class RecycleNodeFileDAO implements RecycleNodeDAO {
 
 
+    /**
+     * getRecycleNode gets all recycle nodes
+     *
+     * @return all nodes in an array
+     * @throws IOException if issue with internal storage
+     */
+    @Override
+    public RecycleNode[] getRecycleNodes() throws IOException {
+        return new RecycleNode[0];
+    }
+
+    /**
+     * getRecycleNode gets a node by id
+     *
+     * @param id recycleid
+     * @return RecycleNode
+     * @throws IOException if issue with internal storage
+     */
+    @Override
+    public RecycleNode getRecycleNode(int id) throws IOException {
+        return null;
+    }
+
+    /**
+     * createRecycleNode creates a new recyclenode
+     *
+     * @param newNode new node to be created
+     * @return created node
+     * @throws IOException if issue with internal storage
+     */
+    @Override
+    public RecycleNode createRecycleNode(RecycleNode newNode) throws IOException {
+        return null;
+    }
+
+    /**
+     * updateRecycleNode updates given node with new information
+     *
+     * @param updatedNode given node
+     * @return updated node
+     * @throws IOException if issue with internal storage
+     */
+    @Override
+    public RecycleNode updateRecycleNode(RecycleNode updatedNode) throws IOException {
+        return null;
+    }
+
+    /**
+     * deleteRecycleNode deletes node with given id
+     *
+     * @param id node id
+     * @return true if node was deleted, false if not
+     * @throws IOException if issue with internal storage
+     */
+    @Override
+    public boolean deleteRecycleNode(int id) throws IOException {
+        return false;
+    }
 }
--- Backend/RecycleRacoonAPI/src/main/java/com/recycleracoon/RecycleRacoonAPI/Persistence/UserFileDAO.java
+++ Backend/RecycleRacoonAPI/src/main/java/com/recycleracoon/RecycleRacoonAPI/Persistence/UserFileDAO.java
@@ -1,5 +1,68 @@
 package com.recycleracoon.RecycleRacoonAPI.Persistence;
 
-public class UserFileDAO implements UserDAO{
+import com.recycleracoon.RecycleRacoonAPI.Model.User;
 
+import java.io.IOException;
+
+public class UserFileDAO implements UserDAO {
+
+    /**
+     * getUsers gets all users
+     *
+     * @return all users in an array
+     * @throws IOException if issue with internal storage
+     */
+    @Override
+    public User[] getUsers() throws IOException {
+        return new User[0];
+    }
+
+    /**
+     * getUser gets a user by ID
+     *
+     * @param id userID
+     * @return User
+     * @throws IOException if issue with internal storage
+     */
+    @Override
+    public User getUser(int id) throws IOException {
+        return null;
+    }
+
+    /**
+     * createUser creates a new user
+     *
+     * @param newUser given user to create
+     * @return created user
+     * @throws IOException if issue with internal storage
+     */
+    @Override
+    public User createUser(User newUser) throws IOException {
+        return null;
+    }
+
+    /**
+     * updateUser updates user with new information
+     * this will be how a new location will be added to user list
+     *
+     * @param updatedUser user with updated information
+     * @return the updated user
+     * @throws IOException if issue with internal storage
+     */
+    @Override
+    public User updateUser(User updatedUser) throws IOException {
+        return null;
+    }
+
+    /**
+     * deleteUser deletes user with given id
+     *
+     * @param id user id
+     * @return true if user was deleted, false if not
+     * @throws IOException if issue with internal storage
+     */
+    @Override
+    public boolean deleteUser(int id) throws IOException {
+        return false;
+    }
 }
