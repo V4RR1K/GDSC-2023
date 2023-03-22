f4b298fed017e73033ca637febfa2981c560e194
mackenziewade <kinkinwade@gmail.com>
2023-03-21 21:32:04 -0400

    nav bar completed

--- Frontend/angular-recycleRaccoon/src/app/app.module.ts
+++ Frontend/angular-recycleRaccoon/src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent
+    NavbarComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
