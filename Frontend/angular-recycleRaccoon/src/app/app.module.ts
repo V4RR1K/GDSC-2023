582ae275ee0bce707708ca5b94ba27ef55729526
mackenziewade <kinkinwade@gmail.com>
2023-03-22 21:37:19 -0400

    homePage completed

--- Frontend/angular-recycleRaccoon/src/app/app.module.ts
+++ Frontend/angular-recycleRaccoon/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 
 
 
@@ -13,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   declarations: [
     AppComponent,
     NavbarComponent,
+    HomeComponent,
 
   ],
   imports: [
